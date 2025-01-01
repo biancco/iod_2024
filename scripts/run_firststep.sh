e37b521ea9dfd5bd12abe7c565980ccdcb035cd5
biancco <biancco@snu.ac.kr>
2025-01-02 00:54:15 +0900

    scripts

--- scripts/run_firststep.sh
+++ scripts/run_firststep.sh
@@ -1,25 +1,12 @@
 #!/bin/bash
-port=$(python tools/get_free_port.py)
-GPU=2
-CUDA_VISIBLE_DEVICES=0,1
-
-#  Choose the corresponding first task #
-# 19 classes in first task #
-#task=19-1
-
-# 15 classes in first task #
-task=15-5
+GPU=1
+CUDA_VISIBLE_DEVICES=0
 
 # 10 classes in first task #
-#task=10-10
-
-# 5 classes in first task #
-# task=5-15
-
+task=10-10
 
 #### 1. Train the First Task ####
-
-python -m torch.distributed.launch --master_port=${port} --nproc_per_node=${GPU} tools/train_first_step.py -c configs/voc/${task}/e2e_faster_rcnn_R_50_C4_4x.yaml -cvd ${CUDA_VISIBLE_DEVICES}
+python tools/train_first_step.py -c configs/voc/${task}/e2e_faster_rcnn_R_50_C4_4x.yaml -cvd ${CUDA_VISIBLE_DEVICES}
 
 #### 2. Prototype Box Selection (PBR) ####
 step=0
